e0eae44310c808a5c2d06c7b682c644acb8b5ca0
Touille <Touille.cat@outlook.fr>
2026-04-20 20:33:58 +0200

    miaou

--- main.cpp
+++ main.cpp
@@ -3,20 +3,28 @@
 //
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "Menu_demarrage.hpp"
 
 int main() {
     // 1. Initialisation de la fenêtre
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Jeu de Quetes - Moteur v1.0");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Jeu de Quetes");
+    std::cout << "Jeu lance" << std::endl;
+    std::string etat("menu_demarrage");
 
     // 2. La Boucle de Jeu Principale
     while (window.isOpen()) {
 
         // 3. Gestion des évènements (Inputs du joueur)
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event)) {                                                                // Gestion des inputs
             // Si le joueur clique sur la croix pour fermer
             if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+            if (etat == "menu_demarrage" && event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                }
             }
         }
 
@@ -25,6 +33,7 @@ int main() {
 
         // 5. Affichage (Render)
         window.clear(sf::Color::Black); // Étape A : On efface l'écran avec du noir
+        dessinerMenu(window);
 
         // Étape B : C'est ici qu'on dessinera nos Gobelins, Orques et le décor !
 
